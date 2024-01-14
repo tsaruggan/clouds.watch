6a5f85b3f4e1c90268f746c2bcfbb7b2740638f3
tsaruggan <tsaruggan@gmail.com>
2024-01-14 00:27:04 -0500

    removed old server stuff

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`listening at port ${PORT}`));
-app.use(express.static('client'));
